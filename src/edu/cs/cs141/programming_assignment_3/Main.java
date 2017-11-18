ead39e51d3d6a5360a2f66c167fb64de0bec6b15
Vincent Torossian <vincenttoro19@gmail.com>
2017-11-18 14:40:59 -0800

    Ur gay

--- src/edu/cs/cs141/programming_assignment_3/Main.java
+++ src/edu/cs/cs141/programming_assignment_3/Main.java
@@ -2,11 +2,14 @@
  * 
  */
 package edu.cs.cs141.programming_assignment_3;
-
+import java.util.Scanner;
 /**
  * @author ttash
  *
  */
 public class Main {
-
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
+	}
 }
