39158c8216cee951b098062961d12169bed562ce
Vincent Torossian <vincenttoro19@gmail.com>
2017-11-18 15:06:45 -0800

    scanner

--- src/edu/cs/cs141/programming_assignment_3/UI.java
+++ src/edu/cs/cs141/programming_assignment_3/UI.java
@@ -2,11 +2,18 @@
  * 
  */
 package edu.cs.cs141.programming_assignment_3;
+import java.util.Scanner;
 
 /**
  * @author ttash
  *
  */
-public class UI {
-
+public class UI 
+{
+	private Scanner input = null;
+	
+	public UI()
+	{
+		Scanner input = new Scanner(System.in);
+	}
 }
