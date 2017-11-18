1a22340b347f718a1381522d3fc806e0d8a5c0fe
Vincent Torossian <vincenttoro19@gmail.com>
2017-11-18 15:15:39 -0800

    dieases

---
+++ src/edu/cs/cs141/programming_assignment_3/Diseases.java
@@ -0,0 +1,12 @@
+package edu.cs.cs141.programming_assignment_3;
+
+public class Diseases {
+	private String name;//name of disease
+	private boolean status; //true for current false for past
+	
+	public Diseases(String aname, boolean astatus)
+	{
+		name = aname;
+		status = astatus;
+	}
+}
