fac5881892d5052c0bc94a6829235fca2f6e67d7
Vincent Torossian <vincenttoro19@gmail.com>
2017-11-18 15:23:00 -0800

    ASDF

--- src/edu/cs/cs141/programming_assignment_3/Animals.java
+++ src/edu/cs/cs141/programming_assignment_3/Animals.java
@@ -2,7 +2,7 @@
  * 
  */
 package edu.cs.cs141.programming_assignment_3;
-
+import java.util.ArrayList;
 /**
  * @author ttash
  *
@@ -11,5 +11,15 @@ public class Animals {
 	private String owner;
 	private String name;
 	private int age;
+	private ArrayList<Diseases> diseases;
+	private ArrayList<String> vaccinations;
 	
+	public Animals(String aowner, String aname, int aage, ArrayList<Diseases> adiseases, ArrayList<String> avaccines)
+	{
+		owner = aowner;
+		name = aname;
+		age = aage;
+		diseases = adiseases;
+		vaccinations = avaccines;
+	}
 }
